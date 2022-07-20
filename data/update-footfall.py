a52beadd59e250ed9b9bd0ede7084a8321630707
Daniel Boam <daniel.boam@open-innovations.org>
2022-07-20 06:31:15 +0100

    Handles exception when file can not be read

--- data/update-footfall.py
+++ data/update-footfall.py
@@ -18,8 +18,12 @@ resources = pd.DataFrame(package['result']['resources'])
 resources.created = resources.created.str[:10]  #strip time section from datetime as we only need date
 resources.created = pd.to_datetime(resources.created, format=ISO_DATE_FORMAT)
 
+
 for k,v in index.items():
   fName = f"{k[6:]}-1.csv"
+  SKIP_FILES = ["footfall-5-1.csv","footfall-8-1.csv"]
+  if fName in SKIP_FILES:
+    continue
 
   filename = path.join(data_dir,fName)
   existing = pd.read_csv(filename)
@@ -28,26 +32,29 @@ for k,v in index.items():
   last_date = existing.tail(n=1).Date.values[0]
   new_resources = resources[resources.created > last_date]
 
-  for i, row in new_resources.iterrows():
-    url = row.url
-    new_data = pd.read_csv(url)
+  try:
+    for i, row in new_resources.iterrows():
+      url = row.url
+      new_data = pd.read_csv(url)
 
-    #cleanup data a bit
-    new_data.Date = pd.to_datetime(new_data.Date, format='%d-%b-%y')
-    new_data.LocationName = new_data.LocationName.str.strip()
-    new_data.LocationName = new_data.LocationName.str.lower()
+      #cleanup data a bit
+      new_data.Date = pd.to_datetime(new_data.Date, format='%d-%b-%y')
+      new_data.LocationName = new_data.LocationName.str.strip()
+      new_data.LocationName = new_data.LocationName.str.lower()
 
-    sitename = v['desc'].strip()
-    sitename = sitename.lower()
-    
-    site_data = new_data[new_data.LocationName == sitename]
-    for date in site_data.Date.unique():
-      by_date = site_data[site_data.Date == date]
-      new_row = {'Date': date}
-      for time in TIMES:
-        by_time = by_date[by_date.Hour == time]
-        new_row[time] = by_time.ReportCount.values[0]
-      existing = pd.concat([existing,pd.DataFrame(new_row,[0])], ignore_index=True)
+      sitename = v['desc'].strip()
+      sitename = sitename.lower()
+      
+      site_data = new_data[new_data.LocationName == sitename]
+      for date in site_data.Date.unique():
+        by_date = site_data[site_data.Date == date]
+        new_row = {'Date': date}
+        for time in TIMES:
+          by_time = by_date[by_date.Hour == time]
+          new_row[time] = by_time.ReportCount.values[0]
+        existing = pd.concat([existing,pd.DataFrame(new_row,[0])], ignore_index=True)
+  except:
+    print("could not process file")
   existing.sort_values(by='Date', inplace=True)
   existing = existing.drop_duplicates()
   existing.to_csv(filename, index=False, date_format=ISO_DATE_FORMAT)
